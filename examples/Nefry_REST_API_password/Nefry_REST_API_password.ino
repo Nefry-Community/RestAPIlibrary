392040c95d016e52b7d46fed6c3b6c056bd252bb
wami <wamiwami@live.jp>
2016-10-17 21:56:39 +0900

    サンプルプログラムを追加

---
+++ examples/Nefry_REST_API_password/Nefry_REST_API_password.ino
@@ -0,0 +1,9 @@
+#include <NefryRESTAPI.h>
+void setup() {
+  NefryRESTAPI.begin();//RESTAPIを使うのに必要な初期化を行います。
+  NefryRESTAPI.setPassword("Nefry");//接続時にパスワードが必要になります。
+}
+
+void loop() {
+
+}
---
+++ examples/Nefry_REST_API_simple/Nefry_REST_API_simple.ino
@@ -0,0 +1,7 @@
+#include <NefryRESTAPI.h>
+void setup() {
+NefryRESTAPI.begin();//RESTAPIを使うのに必要な初期化を行います。
+}
+
+void loop() {
+}
